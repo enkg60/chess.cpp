c7b2ddf6c8fc93d8fec9655c6710ccfca258111f
enkg60 <emiliomateoguevara@gmail.com>
2024-02-24 17:08:27 -0500

    Updated start_game and print_board

--- main.cpp
+++ main.cpp
@@ -1,30 +1,53 @@
 #include <iostream>
 using namespace std;
 
+char board[8][8];
+
 void start_game(){
+
+  char pawns[8] = {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'};
+  char others[8] = {'t', 'h', 'b', 'q', 'k', 'b', 'h', 't'};
+
+  for(int y = 0; y < 8; y++){
+    for(int x = 0; x < 8; x++){
+      if(y == 0 || y == 7){
+        board[y][x] = pawns[x];
+      } else if (y == 1 || y == 6){
+        board[y][x] = others[x];
+      } else {
+        board[y][x] = '+';
+      }
+
+    }
+  }
+  /*
   board = {
-    ['t', 'h', 'b', 'q', 'k', 'b', 'h', 't'],
-    ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
-    ['t', 'h', 'b', 'q', 'k', 'b', 'h', 't'],
+    {'t', 'h', 'b', 'q', 'k', 'b', 'h', 't'},
+    {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+    {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+    {'t', 'h', 'b', 'q', 'k', 'b', 'h', 't'},
   };
+  */
 }
 
 void print_board(){
-  for(int x = 0; x < 8; x++){
-    for(int y = 0; y < ;8 y++){
-      cout << board[x][y];
+  cout << "  a b c d e f g h\n";
+  for(int y = 0; y < 8; y++){
+    for(int x = 0; x < 8; x++){
+      if(x == 0){
+        cout << y+1 << ' ';
+      }
+      cout << board[y][x] << ' ';
     }
     cout << "\n";
   }
 }
 
 int main(){
-  char board[8][8];
   start_game();
   print_board();
 }
