bb654ad18c513c0100436d2f7bdd01be58bf716d
Emilio <emilio@pedros-mbp.lan>
2024-02-24 22:25:33 -0500

    modified:   a.out 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -10,31 +10,29 @@ char display[10][18];
 //chess = 9 tall 17 wide
 */
 
-
-void reset_board(){
+void reset_board() {
 
   char pawns[8] = {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'};
   char others[8] = {'r', 'h', 'b', 'q', 'k', 'b', 'h', 'r'};
 
-  for(int y = 0; y < 8; y++){
-    for(int x = 0; x < 8; x++){
-      if(y == 0 || y == 7){
-        board[y][x] = pawns[x];
-      } else if (y == 1 || y == 6){
+  for (int y = 0; y < 8; y++) {
+    for (int x = 0; x < 8; x++) {
+      if (y == 0 || y == 7) {
         board[y][x] = others[x];
+      } else if (y == 1 || y == 6) {
+        board[y][x] = pawns[x];
       } else {
         board[y][x] = '+';
       }
-
     }
   }
 }
 
-void display_board(){
-  for(int y = 0; y < 8; y++){
-    for(int x = 0; x < 8; x++){
-      if(x == 0){
-        cout << 8-(y) << ' ';
+void display_board() {
+  for (int y = 0; y < 8; y++) {
+    for (int x = 0; x < 8; x++) {
+      if (x == 0) {
+        cout << 8 - (y) << ' ';
       }
       cout << board[y][x] << ' ';
     }
@@ -43,53 +41,47 @@ void display_board(){
   cout << "  a b c d e f g h\n";
 }
 
-bool check_spot_for_piece(int location[2]){
-  if(board[location[0]][location[1]] != '+'){
+bool check_spot_for_piece(int location[2]) {
+  if (board[location[0]][location[1]] != '+') {
     return true;
-  }else{
+  } else {
     return false;
   }
 }
 
-bool check_move(bool white, int location[2], int target[2]){
+bool check_move(bool white, int location[2], int target[2]) {
   int size = 0;
 
   bool pawn, bishop, knight, rook, queen, king;
   pawn = bishop = knight = rook = queen = king = true;
 
-
-  if(pawn){
+  if (pawn) {
   }
 
-
-  if(bishop){
-
+  if (bishop) {
   }
-  if(knight){
-
+  if (knight) {
   }
-  if(rook){
-
+  if (rook) {
   }
-  if(king){
-
+  if (king) {
   }
-  if(queen){
-
+  if (queen) {
   }
 
-  //check if possibleMoves line up with target
-  for(int i = 0; i < size; i++){
-    if(possibleMoves[i] == target){
-      
+  // check if possibleMoves line up with target
+  /*
+  for (int i = 0; i < size; i++) {
+    if (possibleMoves[i] == target) {
+
       return true;
     }
-
   }
+  */
   return false;
 }
 
-int main(){
+int main() {
   reset_board();
   display_board();
   /*
@@ -103,15 +95,15 @@ int main(){
   */
 }
 
-  /*
-  board = {
-    {'t', 'h', 'b', 'q', 'k', 'b', 'h', 't'},
-    {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-    {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-    {'t', 'h', 'b', 'q', 'k', 'b', 'h', 't'},
-  };
-  */
+/*
+board = {
+  {'t', 'h', 'b', 'q', 'k', 'b', 'h', 't'},
+  {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+  {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+  {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+  {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+  {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+  {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+  {'t', 'h', 'b', 'q', 'k', 'b', 'h', 't'},
+};
+*/
