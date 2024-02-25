84b3514029867e77871b540adbf622d413af41e8
enkg60 <emiliomateoguevara@gmail.com>
2024-02-24 19:01:34 -0500

    added check moves

--- main.cpp
+++ main.cpp
@@ -3,10 +3,18 @@ using namespace std;
 
 char board[8][8];
 
+/*
+char display[10][18];
+//10 tall, 18 wide
+
+//chess = 9 tall 17 wide
+*/
+
+
 void reset_board(){
 
   char pawns[8] = {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'};
-  char others[8] = {'t', 'h', 'b', 'q', 'k', 'b', 'h', 't'};
+  char others[8] = {'r', 'h', 'b', 'q', 'k', 'b', 'h', 'r'};
 
   for(int y = 0; y < 8; y++){
     for(int x = 0; x < 8; x++){
@@ -20,21 +28,9 @@ void reset_board(){
 
     }
   }
-  /*
-  board = {
-    {'t', 'h', 'b', 'q', 'k', 'b', 'h', 't'},
-    {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-    {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-    {'t', 'h', 'b', 'q', 'k', 'b', 'h', 't'},
-  };
-  */
 }
 
-void print_board(){
+void display_board(){
   cout << "  a b c d e f g h\n";
   for(int y = 0; y < 8; y++){
     for(int x = 0; x < 8; x++){
@@ -47,10 +43,48 @@ void print_board(){
   }
 }
 
+bool check_moves(char piece){
+  bool pawn, bishop, knight, rook, queen, king;
+  pawn = bishop = knight = rook = queen = king = true;
+  if(pawn){
+
+  }
+  if(bishop){
+
+  }
+  if(knight){
+
+  }
+  if(rook){
+
+  }
+  if(king){
+
+  }
+  if(queen){
+
+  }
+  return false;
+}
+
 int main(){
   reset_board();
   while(true){
     string input;
     cin >> input;
+    
   }
 }
+
+  /*
+  board = {
+    {'t', 'h', 'b', 'q', 'k', 'b', 'h', 't'},
+    {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+    {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+    {'t', 'h', 'b', 'q', 'k', 'b', 'h', 't'},
+  };
+  */
