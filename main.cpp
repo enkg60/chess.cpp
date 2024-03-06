3e9cfa5051c18f626dae4dcb0e87354d867e9e43
enkg60 <emiliomateoguevara@gmail.com>
2024-03-05 20:40:52 -0500

    finished #48

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 // #include <cmath> don't know why this is here
 #include <iostream>
 #include <stack>
+#include <vector>
 #include <sys/wait.h> //why this here
 // p = pawn
 // r = rook
@@ -22,6 +23,7 @@ vector<char> takenWhite;
 struct piece {
   char type;
   int color;
+  int turn;
   // 1 = white, 2 = black and 0 = nothing
 };
 int white = 1;
@@ -33,10 +35,11 @@ piece board[8][8];
 // planned display size: 10 tall, 18 wide
 // chess: 9 tall 17 wide
 
-piece create_piece(char type, int color) {
+piece create_piece(char type, int color, int turn) {
   piece piece;
   piece.type = type;
   piece.color = color;
+  piece.turn = turn; // if turn == 0 then there is no piece there
   return piece;
 }
 
@@ -45,15 +48,15 @@ void reset_board() {
   for (int y = 0; y < 8; y++) {
     for (int x = 0; x < 8; x++) {
       if (y == 0) {
-        board[y][x] = create_piece(pieces[x], black);
+        board[y][x] = create_piece(pieces[x], black, 1);
       } else if (y == 7) {
-        board[y][x] = create_piece(pieces[x], white);
+        board[y][x] = create_piece(pieces[x], white, 1);
       } else if (y == 1) {
-        board[y][x] = create_piece('p', black);
+        board[y][x] = create_piece('p', black, 1);
       } else if (y == 6) {
-        board[y][x] = create_piece('p', white);
+        board[y][x] = create_piece('p', white, 1);
       } else {
-        board[y][x] = create_piece('+', nothing);
+        board[y][x] = create_piece('+', nothing, 0);
       }
     }
   }
@@ -229,14 +232,16 @@ void nerd_display_board(stack<array<int, 2> /* */> possibleMoves) {
 
 bool check_for_piece(array<int, 2> location) {
   if (board[location[0]][location[1]].type != '+') {
+    cout << "IN THE BOUNDS \n";
     return true;
   } else {
+    cout << "NOT IN THE BOUNDS \n";
     return false;
   }
 }
 
 bool piece_in_boundaries(array<int, 2> piece) {
-  if (piece[0] > 7 || piece[0] < 0 || piece[1] > 7 || piece[1] < 0) {
+  if (piece[0] <= 8 && piece[0] >= 0  && piece[1] <= 7 && piece[1] >= 0) {
     return true;
   } else {
     return false;
@@ -255,12 +260,15 @@ stack<array<int, 2> /* */> possible_pawn_moves(int y, int x) {
   array<int, 2> forward = {y - (1 * flip), x};
   array<int, 2> forwardBy2 = {y - (2 * flip), x};
 
+  cout << "\nTURN: " << board[y][x].turn << '\n';
+
   if (!check_for_piece(forward) && piece_in_boundaries(forward)) {
     possibleMoves.push(forward);
+    if (!check_for_piece(forwardBy2) && piece_in_boundaries(forwardBy2) && board[y][x].turn == 1) {
+      possibleMoves.push(forwardBy2);
+    }
   }
-  if (!check_for_piece(forwardBy2) && piece_in_boundaries(forwardBy2)) {
-    possibleMoves.push(forwardBy2);
-  }
+  
   if (x == 0) { // if pawn is on right wall only check diagonal right
     if (check_for_piece(diagonalRight) &&
         board[diagonalRight[0]][diagonalRight[1]].color != board[y][x].color &&
@@ -478,73 +486,6 @@ stack<array<int, 2> /* */> possible_queen_moves(int y, int x) {
     possibleMoves.push(part2.top());
     part2.pop();
   }
-  /*
-  for (int side = 0; side < 8; side++) {
-
-    int iterator = 1;
-    while (true) {
-      array<int, 2> forward;
-      if (side == 0) {
-        if (y == 0) {
-          break;
-        }
-        forward[0] = y - iterator;
-        forward[1] = x;
-      }
-      if (side == 1) {
-        if (y == 7) {
-          break;
-        }
-        forward[0] = y + iterator;
-        forward[1] = x;
-      }
-      if (side == 2) {
-        if (x == 0) {
-          break;
-        }
-        forward[0] = y;
-        forward[1] = x - iterator;
-      }
-      if (side == 3) {
-        if (x == 7) {
-          break;
-        }
-        forward[0] = y;
-        forward[1] = x + iterator;
-      }
-      if (side == 4) { // diagonalUpRight
-        forward[0] = y - iterator;
-        forward[1] = x + iterator;
-      }
-      if (side == 5) { // diagonalUpLeft
-        forward[0] = y - iterator;
-        forward[1] = x - iterator;
-      }
-      if (side == 6) { // diagonalDownRight
-        forward[0] = y + iterator;
-        forward[1] = x + iterator;
-      }
-      if (side == 7) { // diagonalDownLeft
-        forward[0] = y + iterator;
-        forward[1] = x - iterator;
-      }
-
-      if (forward[0] > 7 || forward[0] < 0 || forward[1] > 7 ||
-          forward[1] < 0) {
-        break;
-      }
-
-      if (check_for_piece(forward) == false) {
-        possibleMoves.push(forward);
-        iterator++;
-        continue;
-      } else {
-        possibleMoves.push(forward);
-        break;
-      }
-    }
-  }
-  */
   return possibleMoves;
 }
 
@@ -623,10 +564,13 @@ bool move_piece(array<int, 2> position, array<int, 2> target) {
       // copy over properties of piece to target
       board[targetY][targetX].type = board[positionY][positionX].type;
       board[targetY][targetX].color = board[positionY][positionX].color;
+      board[targetY][targetX].turn = board[positionY][positionX].turn;
+      board[targetY][targetX].turn++;
 
       // replace piece with empty spot
       board[positionY][positionX].type = '+';
       board[positionY][positionX].color = nothing;
+      board[positionY][positionX].turn = 0;
       ifFunctionWorked = true;
       break;
     } else {
